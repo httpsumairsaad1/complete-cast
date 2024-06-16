6862fd0ab782981f12bfbcc21d46542558373c48
Umair Saad <umairsaad52@gmail.com>
2024-06-16 05:39:42 +0500

    Convex FullStack

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,16 +3,24 @@
 // *     *                      DEVELOPER: UMAIR SAAD                 *     *
 // *     *                      COPYRIGHT 2024                        *     *
 // *     *                      DATE: 8 JUNE 2024                     *     *
-// *     *                      UPDATE: 11 JUNE                       *     *
+// *     *                      UPDATE: 16 JUNE                       *     *
 // *     *                                                            *     *
 // *     **************************************************************     *
+
+
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 import React from 'react';
 import {Button} from '@/components/ui/button';
 import { podcastData } from '@/components/constants';
 import PodcastCard from '@/components/PodcastCard';
 
+
+
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex-col gap-9">
       <section className='flex flex-col gap-5'>
@@ -21,6 +29,11 @@ const Home = () => {
         {/* <Button className="text-white-1 bg-orange-2">Button</Button> */}
 
         {/* Data from Constants */}
+
+          <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+            {tasks?.map(({ _id }) => <div key={_id}></div>)}
+          </div>
+        
         <div className='podcast_grid'>
         {
           podcastData.map(({id, title, description, imgURL}) => (
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from "./providers/ConvexClientProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ConvexClientProvider>
+      {children}
+      </ConvexClientProvider>
+      </body>
     </html>
   );
 }
